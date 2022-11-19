cbfdfeeacfd1cbe957979a59e44d54381a818f11
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-11-20 01:19:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,4 +8,15 @@ st.title('About my Project')
 st.sidebar.header(Project_Name)
 st.sidebar.write(Done_By)
 choice=st.sidebar.radio('select Options', Menu_Options)
+
+
+if choice =='Home' :
+    st.image('image/SoftwareInstall.jpeg')
+if choice=='About':
+    st.image('image/Webcamset.jpeg')
+if choice=='Contact':
+    st.image('image/maskgirls.png')    
+if choice=='Services':
+    st.image('image/WhatsApp Image 2021-05-25 at 5.46.51 PM.jpeg')    
+
 st.write('👋','helloharendra')
